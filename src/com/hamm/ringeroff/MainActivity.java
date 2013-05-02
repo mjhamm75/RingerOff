eaa847fed391afd522ef7f1df76f0e911bf6caad
Jason Hamm <mhamm@availity.com>
2013-05-02 07:05:13 -0400

    Add broadcast receiver

--- src/com/hamm/ringeroff/MainActivity.java
+++ src/com/hamm/ringeroff/MainActivity.java
@@ -1,7 +1,12 @@
 package com.hamm.ringeroff;
 
+import com.app.filterforget.OrderAlarmBroadcastReceiver;
+
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +23,12 @@ public class MainActivity extends Activity {
 	}
 	
 	public void start(View v){
+		Intent intent = new Intent(this, AlarmBroadcastReceiver.class);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 234324243, intent, 0);
+		AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+		alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (8 * 1000),
+				pendingIntent);
+		
 		currentVolume = am.getStreamVolume(AudioManager.STREAM_RING);
 		am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
 		System.out.println("Start");
