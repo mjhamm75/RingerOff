c9d25f9ff3ffbd3c36064d1fbfc781fe98af5b6e
Jason Hamm <mhamm@availity.com>
2013-05-04 06:45:00 -0400

    Abstract out the Audio Manger to make easier to read

--- src/com/hamm/ringeroff/MainActivity.java
+++ src/com/hamm/ringeroff/MainActivity.java
@@ -6,23 +6,37 @@ import android.content.Intent;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
-	int currentVolume;
 	AudioManager am;
+	int currentVolume;
+	int itemSelected = 0;
 	private static final String CANCEL_ACTION = "com.hamm.ringeroff.cancel";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		am = (AudioManager)this.getSystemService(Context.AUDIO_SERVICE);
 	}
 
 	public void start(View v) {
-		am = (AudioManager)this.getSystemService(Context.AUDIO_SERVICE);
-		currentVolume = am.getStreamVolume(AudioManager.STREAM_RING);
-		am.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+		Spinner timeSpan = (Spinner) findViewById(R.id.spinner_length);
+		timeSpan.setOnItemSelectedListener(new OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+				itemSelected = position;
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+				itemSelected = 0;
+			}
+
+		});
+		getAudioManager().setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
 	}
 
 	public void stop(View v) {
@@ -30,5 +44,17 @@ public class MainActivity extends Activity {
 		intent.setAction(CANCEL_ACTION);
 		intent.putExtra("com.hamm.ringeroff.volume", currentVolume);
 		sendBroadcast(intent);
+	}
+
+	public int getCurrentVolume() {
+		AudioManager am = getAudioManager();
+		return am.getStreamVolume(AudioManager.STREAM_RING);
+	}
+
+	public AudioManager getAudioManager() {
+		if (am == null) {
+			am = (AudioManager) this.getSystemService(Context.AUDIO_SERVICE);
+		}
+		return am;
 	}
 }
